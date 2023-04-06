b095ee2d2fa2fe1fd091d445a3911aeb8887db84
mroyale <mroyale575@gmail.com>
2023-04-05 22:11:43 -0400

    Update

---
+++ src/day25Methods/FixFormat.java
@@ -0,0 +1,43 @@
+package day25Methods;
+
+public class FixFormat {
+
+    /*
+    Fix Format
+
+create a method will accept a String and return a String in proper format. Proper format is:
+First character starting as uppercase and the rest as lowercase
+
+Ex:
+    Input:
+        jamES
+
+    Output:
+        James
+
+     */
+
+
+public static String FixFormat{String str) {
+
+    return str.charAt(0) + str.substring(
+    )
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/day25Methods/frequencyOfCharacter.java
@@ -0,0 +1,24 @@
+package day25Methods;
+
+public class frequencyOfCharacter {
+
+    public static int frequencyOfChar(String str, char ch) {
+
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch)
+
+                count++;
+
+        }
+    }
+
+return count;
+}
+
+
+
+
+
+
+}
